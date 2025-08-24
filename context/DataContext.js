3eef7096f60d5612a6a644b6e7708493116d3918
nasky <nuridinawing121@gmail.com>
2025-08-24 10:01:26 +0700

    Update DataContext.js

--- context/DataContext.js
+++ context/DataContext.js
@@ -1,1 +1,20 @@
+import { createContext, useState, useContext } from "react";
 
+const DataContext = createContext();
+
+export const DataProvider = ({ children }) => {
+  const [data, setData] = useState({
+    hartaKotor: 0,
+    hutang: 0,
+    wasiat: 0,
+    ahliWaris: {}
+  });
+
+  return (
+    <DataContext.Provider value={{ data, setData }}>
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+export const useData = () => useContext(DataContext);
